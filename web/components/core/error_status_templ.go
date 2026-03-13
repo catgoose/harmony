48be31937d06494c52eb635fa2fe68740fd56aca
Joshua Tye <21010072+catgoose@users.noreply.github.com>
2026-03-13 12:25:15 -0500

    fix: should be using daisyUI components

--- web/components/core/error_status_templ.go
+++ web/components/core/error_status_templ.go
@@ -418,7 +418,7 @@ func InlineErrorPanel(ec hypermedia.ErrorContext) templ.Component {
 			templ_7745c5c3_Var12 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 22, "<div class=\"rounded-lg bg-error text-error-content text-sm p-3 space-y-2\"><div class=\"space-y-1\"><p class=\"font-semibold\"><span class=\"badge badge-sm badge-outline\">")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 22, "<div class=\"alert alert-error rounded-lg text-sm p-3 space-y-2\"><div class=\"space-y-1\"><p class=\"font-semibold\"><span class=\"badge badge-sm badge-outline\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
